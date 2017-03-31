aa1709c5f7d72c10f455c0e4703184713010af59
vaniok123 <ivan.smirnov.95@mail.ru>
2017-03-31 17:56:30 +0300

    add user model

---
+++ hackatonFinal/ISUser.h
@@ -0,0 +1,13 @@
+//
+//  ISUser.h
+//  hackatonFinal
+//
+//  Created by Smirnov Ivan on 31.03.17.
+//  Copyright © 2017 iosBaumanTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ISUser : NSObject
+
+@end
