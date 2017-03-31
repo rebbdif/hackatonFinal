3568f856b329e2cc32fbc38825ae53b7fe104e66
devil1001 <jenek-devil@mail.ru>
2017-03-31 19:41:57 +0300

    fix

---
+++ hackatonFinal/models/ISUser.h
@@ -0,0 +1,17 @@
+//
+//  ISUser.h
+//  hackatonFinal
+//
+//  Created by Smirnov Ivan on 31.03.17.
+//  Copyright © 2017 iosBaumanTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ISUser : NSObject
+
+@property(strong,nonatomic)NSString* login;
+@property(strong,nonatomic)NSString* pasword;
+
+
+@end
---
+++ hackatonFinal/models/ISUser.m
@@ -0,0 +1,13 @@
+//
+//  ISUser.m
+//  hackatonFinal
+//
+//  Created by Smirnov Ivan on 31.03.17.
+//  Copyright © 2017 iosBaumanTeam. All rights reserved.
+//
+
+#import "ISUser.h"
+
+@implementation ISUser
+
+@end
