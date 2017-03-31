e6023b48b8abdde5e52a73afb727c89f67775619
MaxiJunkie <Slavik123>
2017-03-31 18:39:56 +0300

    add CollectionVC

---
+++ hackatonFinal/CollectionVC/MSCollectionLayout.h
@@ -0,0 +1,18 @@
+//
+//  MSCollectionLayout.h
+//  hackatonFinal
+//
+//  Created by Максим Стегниенко on 31.03.17.
+//  Copyright © 2017 iosBaumanTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSCollectionLayout : UICollectionViewLayout
+
+@property (nonatomic ,assign) CGSize cellSize;
+
+
+
+
+@end
