eac6a68cb5b4e19b4ffb15c94d940166667f104e
vaniok123 <ivan.smirnov.95@mail.ru>
2017-04-01 03:24:43 +0300

    blure

--- hackatonFinal/AppDelegate.m
+++ hackatonFinal/AppDelegate.m
@@ -24,6 +24,26 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+//    let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+//    let blurEffectView = UIVisualEffectView(effect: blurEffect)
+//    blurEffectView.frame = window!.frame
+//    blurEffectView.tag = 221122
+//    
+//    self.window?.addSubview(blurEffectView)
+    
+    UIBlurEffect* bl=[UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView* blurEffectView=[[UIVisualEffectView alloc]initWithEffect:bl];
+    blurEffectView.frame=self.window.frame;
+    blurEffectView.tag = 221122;
+    [self.window addSubview:blurEffectView];
+    
+    
+    
+    
+    
+    
+    
 }
 
 
@@ -35,11 +55,13 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     [[self.window viewWithTag:221122] removeFromSuperview];
 }
 
 
